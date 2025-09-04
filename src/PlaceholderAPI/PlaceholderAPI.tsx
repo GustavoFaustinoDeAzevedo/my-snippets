2c70fca38630fe397302d855499621c5807b1168
Gustavo Faustino <32557780+GustavoFaustinoDeAzevedo@users.noreply.github.com>
2025-09-03 21:38:20 -0300

    style: muda a configuração de estilos para texto e barra de rolagem

--- src/PlaceholderAPI/PlaceholderAPI.tsx
+++ src/PlaceholderAPI/PlaceholderAPI.tsx
@@ -38,14 +38,18 @@ const PlaceholderAPI = () => {
 
   return (
     <div className="mx-auto max-w-2xl max-h-screen p-8 border rounded-md border-gray-300 dark:border-gray-600 overflow-y-auto scroll-smooth ">
-      <h2 className="text-2xl font-bold mb-4 ">Posts da Placeholder API</h2>
+      <h2 className="text-2xl font-bold mb-4 text-blue-600 dark:text-blue-500 ">
+        Posts da Placeholder API
+      </h2>
       <ul className="font-mono list-inside flex flex-col gap-6 text-sm/6 text-center sm:text-left">
         {posts.map((post: any) => (
           <li
             key={post.id}
             className="flex flex-col gap-4 mb-2 tracking-[-.01em]  hover:bg-gray-100 dark:hover:bg-gray-800 p-4 rounded-md"
           >
-            <h3 className="font-bold text-lg">{post.title}</h3>
+            <h3 className="font-bold text-lg text-blue-600! dark:text-blue-500!">
+              {post.title}
+            </h3>
             <p>{post.body}</p>
           </li>
         ))}
